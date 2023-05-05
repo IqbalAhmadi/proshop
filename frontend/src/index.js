6419713936215dc2ee5c6ec8d7e49ff21678b0a4
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-05-04 17:37:44 -0700

    added cards details

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -23,7 +23,7 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
 
